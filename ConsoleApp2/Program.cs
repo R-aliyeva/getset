2e0922198166a93d742bd68343d2fd57774e58f8
R-aliyeva <rufanefa@code.edu.az>
2023-05-19 23:32:11 +0400

    new added

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.Design.Serialization;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bank bcard = new bank();
+            bcard.Cardid = "123456";
+            bcard.Cardname = "Kapital Bank";
+            bcard.Cvv = "12";
+           //card.Balance = 10;
+            //bcard.Increasebalance(12);
+            Console.WriteLine($"card name is {bcard.Cardname},card ID is {bcard.Cardid},card CVV is {bcard.Cvv},card balance is {bcard.Balance}");
+        }
+    }
+}
---
+++ ConsoleApp2/bank.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class bank
+    {
+        private string name;
+        private string cardid;
+        private string cardname;
+        private string cvv;
+        private decimal balance;
+
+        public string Name { get;set; }
+        public string Cardid
+        {
+            get
+            {
+                return cardid;
+            }
+            set
+            {
+                if (value.Length == 16)
+                {
+
+                }
+                else 
+                {
+                    Console.WriteLine("cardid must be 16 numbers");
+                }
+            }
+        }     
+        public string Cardname { get;set; }
+        public string Cvv {
+            get
+            { 
+                return cvv;
+            }
+            set
+            {
+                bool kontrol=false;
+                if (value.Length == 3)
+                {
+
+                    for (int i = 0; i < value.Length; i++)
+                    {
+                        bool sayimi = char.IsNumber(value[i]);
+                        if (sayimi)
+                        {
+                        }
+                        else
+                        {
+                            kontrol = true;
+                            break;
+                        }
+                    }
+
+
+                }
+                else {
+                    Console.WriteLine("cvv must be 3 numbers only");
+                }
+                if (kontrol)
+                {
+
+                }
+                else
+                {
+                    Console.WriteLine("cvv must be only numbers");
+                }
+
+            }
+               
+
+            }
+                }
+       //ublic int Balance { get; set; }
+        //public void Increasebalance(decimal bl)
+        //{ 
+        //    balance += bl;
+
+        //}
+
+
+
+
+
+    }
+}
